d8975bbdd4ac7734c6d4cdeef305a46725429030
Ivan Vyatkin <vanya095@gmail.com>
2017-07-02 18:47:33 +0300

    Remove old demo site.

--- src/components/link.js
+++
@@ -1,9 +0,0 @@
-import { Link as RouterLink } from 'react-router-dom'
-
-export default class Link extends React.Component {
-  render() {
-    return (
-      <RouterLink {...this.props} />
-    )
-  }
-}
