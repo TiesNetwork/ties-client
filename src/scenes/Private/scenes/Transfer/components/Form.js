8087980c900f154daa71201ff419c1e566e3ab72
Ivan Vyatkin <vanya095@gmail.com>
2017-08-28 19:55:38 +0300

    Update tranfser component.

--- src/scenes/Private/scenes/Transfer/components/Form.js
+++ src/scenes/Private/scenes/Transfer/components/Form.js
@@ -1,25 +1,21 @@
 /** Components **/
-import Button from '../../../components/Button';
-import Form, { Actions, Input, Recipients } from '../../../components/Form';
+import Button from '../../../../../components/Button';
+import Form, { Actions, Input, Recipients } from '../../../../../components/Form';
 
-class TransferForm extends Component {
-  render() {
-    return (
-      <Form onSubmit={this.props.handleSubmit}>
-        <Recipients label="Recipient:" name="recipient" />
+const TransferForm = ({ handleSubmit }) => (
+  <Form onSubmit={this.props.handleSubmit}>
+    <Recipients label="Recipient:" name="recipient" />
 
-        <Input label="Account Number" name="address" />
-        <Input label="Sum" name="sum" />
+    <Input label="Account Number" name="address" />
+    <Input label="Sum" name="sum" />
 
-        <Actions>
-          <Button type="submit">
-            Send
-          </Button>
-        </Actions>
-      </Form>
-    )
-  }
-}
+    <Actions>
+      <Button type="submit">
+        Send
+      </Button>
+    </Actions>
+  </Form>
+);
 
 export default reduxForm({
   form: 'TransferForm'
--- src/scenes/Private/scenes/Transfer/index.js
+++ src/scenes/Private/scenes/Transfer/index.js
@@ -1,37 +1,20 @@
-/** Actions **/
-import { sendTransfer } from './actions';
-
 /** Components **/
-import Block from '../../components/Block';
+import Block from '../../../../components/Block';
 import TransferForm from './components/Form';
 
-class Transfer extends Component {
-  static propTypes = {
-    handleSubmit: PropTypes.func,
-    recipient: PropTypes.shape({
-      __address: PropTypes.string,
-      name: PropTypes.string,
-      photo: PropTypes.instanceOf(Buffer),
-      surname: PropTypes.string
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <Block title="Make a transfer">
-          <TransferForm
-            initialValues={{
-              address: this.props.recipient && this.props.recipient.__address,
-              recipient: this.props.recipient
-            }}
-            onSubmit={this.props.handleSubmit}
-          />
-        </Block>
-      </div>
-    )
-  }
-}
+const Transfer = ({ handleSubmit, recipient }) => (
+  <div>
+    <Block title="Make a transfer">
+      <TransferForm
+        initialValues={{
+          address: recipient && recipient.__address,
+          recipient: recipient
+        }}
+        onSubmit={handleSubmit}
+      />
+    </Block>
+  </div>
+);
 
 const mapStateToProps = (state, ownProps) => ({
   recipient: state.entities.users[ownProps.location.query.to]
